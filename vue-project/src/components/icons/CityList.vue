1b0b6e90e0673be72763cfc651034e7571754fc1
JMathRA <j.matheusalbuquerque1@gmail.com>
2023-11-15 10:47:19 -0300

    modificações

--- vue-project/src/components/icons/CityList.vue
+++ vue-project/src/components/icons/CityList.vue
@@ -1,43 +1,54 @@
 <template>
     <div v-for="city in savedCities" :key="city.id">
-        <CityCard :city="city" @click="goToCityView(city)"/>
+      <CityCard :city="city" @click="goToCityView(city)" />
     </div>
+  
     <p v-if="savedCities.length === 0">
-        No locations added. To start tracking a location, search in the field above.
+      No locations added. To start tracking a location, search in
+      the field above.
     </p>
-</template>
-
-<script setup>
-import axios from 'axios';
-import { useRoute } from 'vue-router';
-
-const savedCities = ref([])
-const getCities = async() => {
-    if(localStorage.getItem("savedCities")){
-        savedCities.value = JSON.parse(localStorage.getItem("savedCities"))
-    }
-
-    const requests = []
-    savedCities.value.forEach((city) => {
+  </template>
+  
+  <script setup>
+  import axios from "axios";
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+  import CityCard from "./CityCard.vue";
+  
+  const savedCities = ref([]);
+  const getCities = async () => {
+    if (localStorage.getItem("savedCities")) {
+      savedCities.value = JSON.parse(
+        localStorage.getItem("savedCities")
+      );
+  
+      const requests = [];
+      savedCities.value.forEach((city) => {
         requests.push(
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=2725e2874b0771c7ca217e32b0e7edfe&units=imperial`)
-        )
-    })
-
-    const weatherData = await Promise.all(requests)
-
-    weatherData.forEach((value, index) => {
-        savedCities.value[index].weather = value.data
-    })
-}
-await getCities ()
-const router = useRouter()
-const goToCityView = (city) => {
+          axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=2725e2874b0771c7ca217e32b0e7edfe&units=imperial`
+          )
+        );
+      });
+  
+      const weatherData = await Promise.all(requests);
+  
+      weatherData.forEach((value, index) => {
+        savedCities.value[index].weather = value.data;
+      });
+    }
+  };
+  await getCities();
+  
+  const router = useRouter();
+  const goToCityView = (city) => {
     router.push({
-        name: 'cityView',
-        params: { state: city.state, city: city.city },
-        query: { lat: city.coords.lat, lng: city.coords.lng },
-    })
-}
-</script>
-
+      name: "cityView",
+      params: { state: city.state, city: city.city },
+      query: {
+        lat: city.coords.lat,
+        lng: city.coords.lng,
+      },
+    });
+  };
+  </script>
